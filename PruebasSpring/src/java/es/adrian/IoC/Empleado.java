b3de00a8a62cadaa6b4922d312944cd03b9d1565
AdriRodri13 <152196347+AdriRodri13@users.noreply.github.com>
2024-10-30 20:19:02 +0100

    Java Spring

---
+++ PruebasSpring/src/java/es/adrian/IoC/Empleado.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.adrian.IoC;
+
+/**
+ *
+ * @author Admin
+ */
+public class Empleado {
+    public String getTareas(){
+        return "Gestiono las cuestiones relativas a mis empleados de seccion";
+    }
+}
