b13efc1afbf4e45fc14a6322dcc9afafdfbe8f23
AdriRodri13 <152196347+AdriRodri13@users.noreply.github.com>
2024-11-08 17:28:32 +0100

    Ouss

--- MavenSpring/src/main/java/es/adrian/IoC/DirectorEmpleado.java
+++ MavenSpring/src/main/java/es/adrian/IoC/DirectorEmpleado.java
@@ -16,6 +16,35 @@ public class DirectorEmpleado implements Empleados{
         this.informe = informe;
     }
     
+    private CreacionInformes informeNuevo;
+    
+    private String email;
+    
+    private String nombreEmpresa;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public CreacionInformes getInformeNuevo() {
+        return informeNuevo;
+    }
+
+    public void setInformeNuevo(CreacionInformes informeNuevo) {
+        this.informeNuevo = informeNuevo;
+    }
     
     
     @Override
